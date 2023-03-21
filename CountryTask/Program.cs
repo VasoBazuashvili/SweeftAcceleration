685524151738e07c99624ef4f1d16ecfd2687a3b
Bazu98 <v.bazuashvili@gmail.com>
2023-03-21 02:13:14 -0700

    added await

--- CountryTask/Program.cs
+++ CountryTask/Program.cs
@@ -4,31 +4,37 @@ using System.Diagnostics.Metrics;
 
 public class Program
 {
-	public static void Main(string[] args)
+	public static async Task Main(string[] args)
 	{
-		GenerateCountryDataFiles();
+		await GenerateCountryDataFiles();
 		Console.WriteLine("Done");
 	}
 
-	public static async void GenerateCountryDataFiles()
+	public static async Task GenerateCountryDataFiles()
 	{
-		Console.WriteLine($"Current Directory: {Directory.GetCurrentDirectory()}");
-
-		var httpClient = new HttpClient();
-		var response = await httpClient.GetAsync("https://restcountries.com/v3.1/all");
-		var content = await response.Content.ReadAsStringAsync();
-		var countries = JsonConvert.DeserializeObject<Country[]>(content);
-		response.EnsureSuccessStatusCode();
-
-		if (response.IsSuccessStatusCode)
+		try
 		{
-			foreach (var country in countries)
-			{
-				var fileName = $"{country.Name.Common}.txt";
-				var text = $"Region: {country.Region}\nSubregion: {country.Subregion}\nLatlng: {string.Join(",", country.Latlng)}\nArea: {country.Area}\nPopulation: {country.Population}";
+			Console.WriteLine($"Current Directory: {Directory.GetCurrentDirectory()}");
 
-				File.WriteAllText(fileName, text);
+			var httpClient = new HttpClient();
+			var response = await httpClient.GetAsync("https://restcountries.com/v3.1/all");
+			var content = await response.Content.ReadAsStringAsync();
+			var countries = JsonConvert.DeserializeObject<Country[]>(content);
+			response.EnsureSuccessStatusCode();
+
+			if (response.IsSuccessStatusCode)
+			{
+				foreach (var country in countries)
+				{
+					var fileName = $"{country.Name.Common}.txt";
+					var text = $"Region: {country.Region}\nSubregion: {country.Subregion}\nLatlng: {string.Join(",", country.Latlng)}\nArea: {country.Area}\nPopulation: {country.Population}";
+					File.WriteAllText(fileName, text);
+				}
 			}
+		}
+		catch
+		{
+			Console.WriteLine("");
 		}
 	}
 	public class Country
